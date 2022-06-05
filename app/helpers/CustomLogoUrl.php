ca003e65002f2d7beeffa26be100fc3a4bc74bc3
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2022-06-05 21:13:20 +0200

    add custom logo in react and setup nav

---
+++ app/helpers/CustomLogoUrl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Get custom logo url.
+ */
+
+function get_custom_logo_url()
+{
+    $custom_logo_id = get_theme_mod( 'custom_logo' );
+    $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+    return $image[0];
+}
--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Link } from '@inertiajs/inertia-react';
 
 const Header = (props) => {
-    console.log(props)
+    const nav = props.data.primary_menu;
+    const logo = props.data.custom_logo;
 
     return (
-        <header className='nav'>
-            {props.data.map((link, index) =>
-                <Link href={link.link} key={index}>{link.name}</Link>
-            )}
+        <header className='nav flex flex-row justify-between'>
+            <Link href='/'>
+                <img src={logo} alt='site logo' className="logo" />
+            </Link>
+            <nav className='nav__menu'>
+                {nav.map((link, index) =>
+                    <Link href={link.link} key={index}>{link.name}</Link>
+                )}
+            </nav>
         </header>
     )
 }
