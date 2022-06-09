e3683934dff484010c4b3afdef8e86a7e973be4e
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2022-06-09 22:21:58 +0200

    use inertia shared data to setup Header component

--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -1,33 +1,19 @@
 import React from 'react';
-import { Link, Head } from '@inertiajs/inertia-react';
+import { Link, usePage } from '@inertiajs/inertia-react';
 import defaultLogo from '../../assets/images/logo-nm.svg';
 
-const Header = (props) => {
-    console.log(props.seo)
+const Header = () => {
+    const { custom_logo } = usePage().props
+    const { primary_menu } = usePage().props
+
     return (
         <>
-
-            {(props.seo.yoast_status === false) &&
-                <Head>
-                    <title>{props.title}</title>
-                </Head>
-            }
-
-            {(props.seo.yoast_status === true) &&
-                <Head>
-                    <meta name="robots" content={props.seo.robots.index + ',' + props.seo.robots.follow}></meta>
-                    <title>{props.seo.seo_title}</title>
-                    <meta name="description" content={props.seo.seo_description} />
-                </Head>
-            }
-
-
             <header className='nav flex flex-row justify-between items-center py-3'>
                 <Link href='/'>
-                    <img src={props.logo != null && props.logo || defaultLogo} alt='site logo' className="logo" />
+                    <img src={custom_logo != null && custom_logo || defaultLogo} alt='site logo' className="logo" />
                 </Link>
                 <nav className='nav-menu'>
-                    {props.nav.map((link, index) =>
+                    {primary_menu.map((link, index) =>
                         <Link href={link.link} key={index}>{link.name}</Link>
                     )}
                 </nav>
--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -4,22 +4,40 @@ import Header from '../Components/Shared/Header'
 import Footer from '../Components/Shared/Footer'
 
 const Index = (props) => {
+    console.log(props)
     return (
 
-        <div className='container mx-auto px-2'>
-            <Header nav={props.primary_menu} logo={props.custom_logo} seo={props.yoast_meta} />
-            <h1>{props.title}</h1>
-            <div className='content' dangerouslySetInnerHTML={{ __html: props.content }} ></div>
-            <div className="post-loop py-3">
-                {props.posts.map((post, index) =>
-                    <article className='post py-2' key={index}>
-                        <h2>{post.post_title}</h2>
-                        <Link href={'/' + post.post_name}>Read more</Link>
-                    </article>
-                )}
+        <>
+            {(props.yoast_status === false) &&
+                <Head>
+                    <title>{props.title}</title>
+                </Head>
+            }
+
+            {(props.yoast_status === true) &&
+                <Head>
+                    <meta name="robots" content={props.robots.index + ',' + props.robots.follow}></meta>
+                    <title>{props.seo_title}</title>
+                    <meta name="description" content={props.seo_description} />
+                </Head>
+            }
+
+            <div className='container mx-auto px-2'>
+                <Header />
+                <h1>{props.title}</h1>
+                <div className='content' dangerouslySetInnerHTML={{ __html: props.content }} ></div>
+                <div className="post-loop py-3">
+                    {props.posts.map((post, index) =>
+                        <article className='post py-2' key={index}>
+                            <h2>{post.post_title}</h2>
+                            <Link href={'/' + post.post_name}>Read more</Link>
+                        </article>
+                    )}
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+
+        </>
     )
 }
 
