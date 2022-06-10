d2a141169bf5db34e3f42ce6c9b3dfcf86530d54
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2022-06-10 20:26:37 +0200

    AppHead component, we need all meta tags being shared over inertia, then remove meta tags from php head

---
+++ src/Components/Shared/AppHead.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Head } from '@inertiajs/inertia-react';
+
+const AppHead = ({ seo, pageTitle }) => {
+    console.log(seo)
+    return (
+        <>
+            {(seo.yoast_status === false) &&
+                <Head>
+                    <title>{pageTitle}</title>
+                    <meta name="description" content={seo.seo_description} />
+                </Head>
+            }
+
+            {(seo.yoast_status === true) &&
+                <Head>
+                    <meta name="robots" content={seo.robots.index + ',' + seo.robots.follow}></meta>
+                    <title>{seo.seo_title}</title>
+                    <meta name="description" content={seo.seo_description} />
+                </Head>
+            }
+        </>
+    )
+}
+
+export default AppHead
--- src/Pages/Page.jsx
+++ src/Pages/Page.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import AppHead from '../Components/Shared/AppHead';
 import Header from '../Components/Shared/Header'
 import Footer from '../Components/Shared/Footer'
 
 const Page = (props) => {
     return (
-        <div className='container mx-auto px-2'>
-            <Header />
-            <h1>{props.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: props.content }} ></div>
-            <Footer />
-        </div>
+
+        <>
+            <AppHead seo={props.yoast_meta} pageTitle={props.title} />
+
+            <div className='container mx-auto px-2'>
+                <Header />
+                <h1>{props.title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: props.content }} ></div>
+                <Footer />
+            </div>
+
+        </>
     )
 }
 
