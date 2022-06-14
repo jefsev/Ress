c8ae538a5a8eee7ed5b89956d9a6d87ef2274dbf
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2022-06-14 19:41:00 +0200

    filter get_the_content so it returns proper HTML tags

--- app/controllers/Page.php
+++ app/controllers/Page.php
@@ -6,10 +6,13 @@ use Ress\Inc\CollectYoastMeta;
 
 class Page {
     public static function index() {
+        // filter get_the_content so it return proper HTML tags
+        $content = apply_filters( 'the_content', get_the_content());
+
         return Inertia::render('Page', [
             'yoast_meta' => CollectYoastMeta::collectMeta(get_the_ID()),
             'title' => get_the_title(),
-            'content' => get_the_content(),
+            'content' => $content,
         ]);
     }
 }
