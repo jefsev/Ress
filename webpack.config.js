dcb305aa3fa3cccce5aee0a4d4be4760250390dd
jefsev <sevinga@protonmail.com>
2022-06-22 20:15:44 +0200

    better webpack, import scss in jsx components

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,61 @@
 const path = require('path');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     plugins: [
         new WebpackManifestPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/app.css",
+            chunkFilename: "[name].css"
+        }),
     ],
 
     entry: [
         path.join(__dirname, 'src', 'app.js'),
-        path.join(__dirname, 'src/assets/scss/', 'app.scss'),
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
-          {
-            test: /\.jsx?$/, 
-            resolve: {
-              extensions: [".js", ".jsx"]
-            },
-            include: path.resolve(__dirname, 'src'),
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', {
-                          "targets": "defaults" 
-                        }],
-                        '@babel/preset-react'
-                      ]
+            {
+                test: /\.jsx?$/,
+                resolve: {
+                    extensions: [".js", ".jsx"]
+                },
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                "targets": "defaults"
+                            }],
+                            '@babel/preset-react'
+                        ]
                     }
                 }
             },
             {
                 test: /\.s[ac]ss$/i,
-                exclude: /node_modules/,
                 use: [
+                    MiniCssExtractPlugin.loader,
                     {
-                        loader: 'file-loader',
-                        options: { outputPath: 'css/', name: '[name].css'}
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            url: true,
+                        }
                     },
-                  'sass-loader', // compiles Sass to CSS, using Node Sass by default
-                  'postcss-loader' // post process the compiled CSS
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    'postcss-loader'
                 ]
             },
             {
@@ -52,5 +64,4 @@ module.exports = {
             },
         ],
     },
-
 }
